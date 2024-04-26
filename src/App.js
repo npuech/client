f3a642f5afcd8de2ba133feb195c9f0dbe7ecc49
Noa Puech <noapuech05@gmail.com>
2024-04-26 17:12:24 +0200

    Modif pour multipage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from "react";
 import "./index.css";
-import NavBar from "./composents/NavBar";
-import Accueil from "./pages/Accueil";
-import Contact from "./pages/contact";
-import Jeux from "./pages/Jeux";
-import QuizzLuxe from "./pages/QuizzLuxe";
-import Mentions from "./pages/MentionsLegales";
-import Footer from "./composents/Footer";
+import { HashRouter as Routes, Route } from "react-router-dom"
+import { NavBar } from "./composents/NavBar";
+import { Accueil } from "./pages/Accueil";
+import { Contact } from "./pages/contact";
+import { Jeux } from "./pages/Jeux";
+import { QuizzLuxe } from "./pages/QuizzLuxe";
+import { Mentions } from "./pages/MentionsLegales";
+import { Footer } from "./composents/Footer";
 
 function App() {
+
   return (
-    <div className="App">
-      <NavBar />
-      <Accueil />
-      <Footer />
-      <Jeux />
-      <Contact />
-      <QuizzLuxe />
-      <Mentions />
+    <div>
+
     </div>
   );
+
 }
 
 export default App;
